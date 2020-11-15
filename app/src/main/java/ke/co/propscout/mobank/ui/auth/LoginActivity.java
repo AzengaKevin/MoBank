597d740d5191fa584695d5a95f08a7abbbb0cc8a
Azenga Kevin <azenga.kevin7@gmail.com>
2020-11-15 08:20:09 +0300

    Wip, login layout implementation and a little bit tweaking of themes

--- app/src/main/java/ke/co/propscout/mobank/ui/auth/LoginActivity.java
+++ app/src/main/java/ke/co/propscout/mobank/ui/auth/LoginActivity.java
@@ -5,21 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import ke.co.propscout.mobank.databinding.ActivityLoginBinding;
 import ke.co.propscout.mobank.ui.HomeActivity;
 import ke.co.propscout.mobank.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private ActivityLoginBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+
+        binding.loginButton.setOnClickListener(v -> startActivity(new Intent(this, HomeActivity.class)));
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-
-        startActivity(new Intent(this, HomeActivity.class));
     }
 }
