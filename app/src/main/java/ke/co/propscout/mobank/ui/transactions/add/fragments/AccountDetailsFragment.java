d7c5e98b3bf77323af4b1c34ff51acce79516aae
Azenga Kevin <azenga.kevin7@gmail.com>
2020-11-19 20:26:40 +0300

    Wip, general layouts done, and rethinking and drying of the app

---
+++ app/src/main/java/ke/co/propscout/mobank/ui/transactions/add/fragments/AccountDetailsFragment.java
@@ -0,0 +1,20 @@
+package ke.co.propscout.mobank.ui.transactions.add.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ke.co.propscout.mobank.R;
+
+public class AccountDetailsFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_account, container, false);
+    }
+}
---
+++ app/src/main/java/ke/co/propscout/mobank/ui/transactions/add/fragments/CustomerDetailsFragment.java
@@ -0,0 +1,21 @@
+package ke.co.propscout.mobank.ui.transactions.add.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ke.co.propscout.mobank.R;
+
+public class CustomerDetailsFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_customer, container, false);
+    }
+}
