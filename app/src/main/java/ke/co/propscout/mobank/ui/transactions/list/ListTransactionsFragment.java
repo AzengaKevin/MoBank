ae4f77373e95bbe4a51f60d58223c84658c78848
Azenga Kevin <azenga.kevin7@gmail.com>
2020-11-19 23:33:25 +0300

    Wip, nested navigation graphs playout

--- app/src/main/java/ke/co/propscout/mobank/ui/transactions/list/ListTransactionsFragment.java
+++ app/src/main/java/ke/co/propscout/mobank/ui/transactions/list/ListTransactionsFragment.java
@@ -1,21 +1,42 @@
 package ke.co.propscout.mobank.ui.transactions.list;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
 import ke.co.propscout.mobank.R;
+import ke.co.propscout.mobank.databinding.FragmentListTransactionsBinding;
 
 public class ListTransactionsFragment extends Fragment {
+
+    private FragmentListTransactionsBinding binding;
+    private NavController navController;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list_transactions, container, false);
+
+        binding = FragmentListTransactionsBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        navController = Navigation.findNavController(view);
+
+        binding.addTransactionFab.setOnClickListener(v -> {
+            //Navigate to add a transaction
+            navController.navigate(R.id.action_add_transaction);
+        });
+
     }
 }
